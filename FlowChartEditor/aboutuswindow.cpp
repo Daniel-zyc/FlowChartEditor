6f2f8a1bfc8d2ef3f360a82c90088a87fe818f30
YoooFan <yooo_fan@163.com>
2024-09-09 13:54:36 +0800

    添加了关于我们界面的内容

--- FlowChartEditor/aboutuswindow.cpp
+++ FlowChartEditor/aboutuswindow.cpp
@@ -10,16 +10,19 @@ AboutUsWindow::AboutUsWindow(QWidget *parent) : QDialog(parent) {
     // 使用 HTML 格式来显示带有链接的文本
     QString markdownText = R"(
         <h1>关于我们</h1>
-        <p><b>欢迎</b>访问关于我们. 这是北京理工大学2022级软件工程大三小学期实践项目</p>
-        <p>不啦不啦不啦不啦不啦不啦不啦不啦不啦不啦不啦不啦，
+        <p>欢迎访问关于我们. 北京理工大学2022级软件工程大三小学期实践项目</p>
+        <p>本项目采用Qt6.4.2，使用MinGW构建，
             通过<a href='https://gitee.com/daniel-zyc/flow-chart-editor.git'>访问</a>
             我们的gitee代码仓库获取源代码</p>
-        <ul>
-            <li>不啦不啦不啦</li>
-            <li>不啦不啦不啦</li>
-            <li>不啦不啦不啦</li>
+        <ul>组员:
+            <li>赵一辰</li>
+            <li>崔琛浩</li>
+            <li>杨林帆</li>
+            <li>郑佳娴</li>
+            <li>张润宁</li>
+            <li>宋卓一</li>
         </ul>
-        <p>感谢</p>
+        <p>2024.8.30 - 2024.9.9</p>
     )";
 
     // 使用 QLabel 来显示文本，并设置为富文本格式
